1dfe45c285a52e1e3e15dcd6fd3c13b0470087c3
OmarDIOP0 <odiop6170@gmail.com>
2025-11-17 21:45:00 +0000

    initial commit

---
+++ P2FixAnAppDotNetCode/Startup.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using P2FixAnAppDotNetCode.Models;
+using P2FixAnAppDotNetCode.Models.Repositories;
+using P2FixAnAppDotNetCode.Models.Services;
+
+namespace P2FixAnAppDotNetCode
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Ajouter les services au conteneur
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Localisation
+            services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
+
+            // Dépendances métier
+            services.AddSingleton<ICart, Cart>();
+            services.AddSingleton<ILanguageService, LanguageService>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IOrderService, OrderService>();
+            services.AddTransient<IOrderRepository, OrderRepository>();
+
+            // Mémoire et session
+            services.AddMemoryCache();
+            services.AddSession();
+
+            // Contrôleurs avec vues et localisation
+            services.AddControllersWithViews()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix,
+                    opts => { opts.ResourcesPath = "Resources"; })
+                .AddDataAnnotationsLocalization();
+
+            // Configuration de la localisation
+            services.Configure<RequestLocalizationOptions>(opts =>
+            {
+                var supportedCultures = new List<CultureInfo>
+                {
+                    new CultureInfo("en-GB"),
+                    new CultureInfo("en-US"),
+                    new CultureInfo("en"),
+                    new CultureInfo("fr-FR"),
+                    new CultureInfo("fr"),
+                };
+
+                opts.DefaultRequestCulture = new RequestCulture("en");
+                opts.SupportedCultures = supportedCultures;
+                opts.SupportedUICultures = supportedCultures;
+            });
+        }
+
+        // Configurer le pipeline HTTP
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            // Localisation
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
+
+            app.UseRouting();
+            app.UseSession();
+            app.UseAuthorization(); // utile si tu as des [Authorize]
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Product}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
